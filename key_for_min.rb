68e9fc6d479ee9e447f322b65cbf61b67284600f
scruffy-node-9597 <gordonbrian16@gmail.com>
2020-06-12 03:58:08 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-low_value = 4
+low_value = 501
 low_value_key = nil
 name_hash.each do |key, value|
     if low_value > value
